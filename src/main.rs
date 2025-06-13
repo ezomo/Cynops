1b3891621cee570245218ce97a2deb0bb0834e93
ezomo <ezomo1daniel@gmail.com>
2025-06-13 12:15:47 +0900

    token追加

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,3 @@
+mod token;
+
 fn main() {}
--- src/token.rs
+++ src/token.rs
@@ -1,1 +1,32 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    Ident(String),
+    Num(usize),
+    Char(char),
+    Plus,             // +
+    Minus,            // -
+    Asterisk,         // *
+    Slash,            // /
+    Equal,            // =
+    LParen,           // (
+    RParen,           // )
+    LBrace,           // {
+    RBrace,           // }
+    Semicolon,        // ;
+    Comma,            // ,
+    Keyword(Keyword), // int, return, if, etc.
+    Eof,
+}
 
+// 予約語
+#[derive(Debug, Clone, PartialEq)]
+pub enum Keyword {
+    Int,
+    Char,
+    Return,
+    If,
+    Else,
+    While,
+    For,
+    Void,
+}
