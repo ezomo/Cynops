956d73f2a35bc5c0f7f8e20cf7207c98a8439733
ezomo <ezomo1daniel@gmail.com>
2025-07-10 17:59:13 +0900

    定義

--- src/sema.rs
+++ src/sema.rs
@@ -1,1 +1,33 @@
+use crate::ast::Ident;
 
+struct Array {
+    array_of: Type,
+    lenght: usize,
+}
+
+struct Func {
+    return_type: Option<Box<Type>>,
+    param: Vec<Type>,
+}
+
+struct Typedef {
+    type_name: Ident,
+    actual_type: Box<Type>,
+}
+
+enum Type {
+    Int,
+    Double,
+    Char,
+    Func(Func),
+    Struct(Vec<Type>),
+    Union(Vec<Type>),
+    Typedef(Typedef),
+    Enum(Vec<Ident>),
+    Pointer(Box<Type>),
+}
+impl Type {
+    fn pointer(ty: Self) -> Self {
+        Self::Pointer(Box::new(ty))
+    }
+}
