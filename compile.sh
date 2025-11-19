1c3433393711fa8eae1d148b918efd4285c1000c
ezomo <ezomo1daniel@gmail.com>
2025-11-19 13:34:42 +0900

    更新

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 出力ディレクトリを作成
-
-
-
-# cargoを実行して出力をファイルに書き込む
-llc ./out/result.ll
-clang ./out/result.s -o ./out/result
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 引数がない場合はエラー
+# # 引数がない場合はエラー
 if [ -z "$1" ]; then
   echo "Usage: $0 <input>"
   exit 1
@@ -11,13 +11,18 @@ rm -rf ./out/
 mkdir -p ./out/
 
 # cargoを実行してLLVM IRを生成
-cargo run "$1" codegen > ./out/result.ll
+cargo run "$1" codegen > ./out/result.bf
+gcc -DMASK=2 hydrogen.c -o brainck
+./brainck ./out/result.bf
 
-opt -O2 ./out/result.ll -S -o ./out/optimized.ll
-# アセンブリ生成 & 実行ファイル作成
-llc ./out/optimized.ll
-clang ./out/optimized.s -o ./out/result
+# lli ./out/result.ll
 
-# 実行 & 終了コード表示
-./out/result
-# echo $?
+# # # アセンブリ生成 & 実行ファイル作成
+# opt -O3 ./out/result.ll -S -o ./out/optimized.ll
+# # lli ./out/optimized.ll
+# llc ./out/result.ll
+# clang ./out/optimized.s -o ./out/result
+# # # lli ./out/optimized.ll
+# # # 実行 & 終了コード表示
+# ./out/result
+# # # echo $?
